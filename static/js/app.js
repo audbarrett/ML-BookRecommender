c1a805010ccdb4ad9bc6105ad8e152473bcd062e
Audrey Barrett <audreybarrett@audreys-mbp.lan>
2024-02-22 21:21:52 -0600

    changes to html

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,19 @@
-// press submit button on enter
-// Get the input field
-var input = document.getElementById("myInput");
-
-// Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("submit_button").click();
+function dropdownFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("bookOptions");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
   }
-});
-
