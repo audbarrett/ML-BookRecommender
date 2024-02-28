0f6f9991d6fb618d9270b38264505fd3c9ffdc25
Audrey Barrett <audbarrett@gmail.com>
2024-02-27 18:37:57 -0600

    updates to html and styling

--- static/js/app.js
+++ static/js/app.js
@@ -1,48 +1,53 @@
 function dropdownFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("bookOptions");
-    li = ul.getElementsByTagName('li');
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("bookOptions");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
     }
   }
 
-  function dropdownFunctionModel() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchInput-model');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("bookOptions-model");
-    li = ul.getElementsByTagName('li');
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+  // Show dropdown menu if there is a search query
+  ul.style.display = filter ? 'block' : 'none';
+}
+
+function dropdownFunctionModel() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchInput-model');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("bookOptions-model");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
     }
   }
 
-  function Sidebaropen(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'flex';
-  }
-  function Sidebarclose(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none';
+  // Show dropdown menu if there is a search query
+  ul.style.display = filter ? 'block' : 'none';
+}
+
+function Sidebaropen(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function Sidebarclose(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
 }
